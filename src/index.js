33a85ecf86016a08243819064876b7ab6b792d50
Andrew <AND2181591@maricopa.edu>
2023-02-22 00:08:22 -0700

    Set up message events to take place when a new user enters or leaves the chat.

--- src/index.js
+++ src/index.js
@@ -16,9 +16,14 @@ io.on('connection', (socket) => {
     console.log('New WebSocket connnection');
 
     socket.emit('message', 'Welcome!');
+    socket.broadcast.emit('message', 'A new user has joined!');
 
     socket.on('sendMessage', (m) => {
         io.emit('message', m);
+    });
+
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left!');
     });
 });
 
