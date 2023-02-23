6d83141bc09e865453c694c2ec4a0c0167a22e39
Andrew <AND2181591@maricopa.edu>
2023-02-23 00:09:53 -0700

    Added event acknowledgements.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
+const Filter = require('bad-words');
 
 const app = express();
 const server = http.createServer(app);
@@ -18,13 +19,20 @@ io.on('connection', (socket) => {
     socket.emit('message', 'Welcome!');
     socket.broadcast.emit('message', 'A new user has joined!');
 
-    socket.on('sendMessage', (m) => {
+    socket.on('sendMessage', (m, callback) => {
+        const filter = new Filter();
+
+        if (filter.isProfane(m)) {
+            return callback('Profanity is not allowed!');
+        }
+
         io.emit('message', m);
+        callback()
     });
 
-    socket.on('sendLocation', (location) => {
-        console.log(location)
-        io.emit('message', `https://google.com/maps?q=${location.latitude},${location.longitude}`)
+    socket.on('sendLocation', (location, callback) => {
+        io.emit('message', `https://google.com/maps?q=${location.latitude},${location.longitude}`);
+        callback();
     });
 
     socket.on('disconnect', () => {
